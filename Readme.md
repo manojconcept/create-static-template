3e64dd755a5e23ba3fa2d9496c72771d7178020b
manojconcept <manojveerapavu@gmail.com>
2024-03-18 14:48:10 +0530

    init

---
+++ Readme.md
@@ -0,0 +1,15 @@
+### Instruction
+
+### RUN
+>`node create create-static-template.js` 
+
+### Folder structure is created
+```
+static-template
+|-- index.html
+`-- src
+    |-- image
+    |-- script
+    |   `-- script.js
+    `-- style
+        `-- style.css
---
+++ create-static-template.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+
+const htmlTemData = [
+    {
+        code: `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <!-- manojconcept v0.01 -->
+            <link rel="stylesheet" href="src/style/style.css">
+        </head>
+        <body>
+            <script src="./src/script/script.js"></script>
+        </body>
+        </html>`,
+        directory: `static-template/index.html`
+    },
+    {
+        code : "",
+        directory: `static-template/src/style/style.css`
+    },
+    {
+        code: "console.log(linked)",
+        directory: `static-template/src/script/script.js`
+    }
+];
+
+//>> To Add Folders
+const directoryPath = ["script", "style", "image"]; 
+
+//>> Function to create a directory
+function createFolder(directory) { 
+    return new Promise((resolve, reject) => {
+        fs.mkdir(directory, { recursive: true }, (err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+}
+
+//>> Function to write a file
+function createFile(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(data.directory, data.code, (err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+}
+
+//>> Sequentially execute actions
+async function executeActions() {
+    try {
+
+        //>> Create folders
+        for (const dir of directoryPath) {
+            await createFolder(`static-template/src/${dir}`);
+            console.log(`Folder created: static-template/src/${dir}`);
+        }
+
+        //>> Create files
+        for (const fileData of htmlTemData) {
+            await createFile(fileData);
+            console.log(`File created: ${fileData.directory}`);
+        }
+
+        console.log("All actions executed successfully.");
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+executeActions();
